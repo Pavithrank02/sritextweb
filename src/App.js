6142faf965f6ab28cd8860d6cd5f2b0bb4934e65
Pavithrank02 <pavithrank02@gmail.com>
2024-12-02 13:35:14 +0530

    view pages setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './view/Home';
+import AboutUs from './view/AboutUs';
+import Contact from './view/Contact';
+import Product from './view/Product';
+import Gallery from './view/Gallery';
 
 function App() {
   return (
     <div className="App">
-     
+      <Routes>
+        <Route path='/' Component={<Home />}/>
+        <Route path='/about' Component={<AboutUs />}/>
+        <Route path='/contact' Component={<Contact />}/>
+        <Route path='/gallery' Component={<Gallery />}/>
+        <Route path='/product' Component={<Product />}/>
+      </Routes>
     </div>
   );
 }
