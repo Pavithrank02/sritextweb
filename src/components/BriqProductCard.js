bdda745deab9e487966bc9b08198aff86e2d37cd
Pavithrank02 <pavithrank02@gmail.com>
2024-12-28 15:36:50 +0530

    updated

--- src/components/BriqProductCard.js
+++ src/components/BriqProductCard.js
@@ -9,6 +9,28 @@ export function BriqProductCard() {
       products: [
         {
           title: "Charcoal Briquette Machine",
+          modalData : [
+            {
+              src: "https://imagekit.io/player/embed/wratuu6c1/IMG_1113.MP4?updatedAt=1735290484777&thumbnail=https%3A%2F%2Fik.imagekit.io%2Fwratuu6c1%2FIMG_1113.MP4%2Fik-thumbnail.jpg%3FupdatedAt%3D1735290484777&updatedAt=1735290484777",
+              type:"video"
+            },
+            {
+              
+              src: "https://5.imimg.com/data5/RU/XD/MY-2995798/centrifugal-air-blower-500x500.jpg",
+            },
+            {
+              
+              src: "https://images.unsplash.com/photo-1623582854588-d60de57fa33f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            },
+            {
+              
+              src: "https://5.imimg.com/data5/EF/RX/MY-2995798/industrial-air-blower-500x500.jpg",
+            },
+            {
+              
+              src: "https://ik.imagekit.io/wratuu6c1/blower%203.PNG?updatedAt=1735291707025",
+            },
+          ],
           src: "https://ik.imagekit.io/wratuu6c1/briquet.png?updatedAt=1735279723945",
           details: [
             ["Price", "Get Quote"],
--- src/components/GalleryShow.js
+++ src/components/GalleryShow.js
@@ -4,33 +4,59 @@ import { IconMinus, IconPlus } from "@tabler/icons-react";
 import { ParallaxScroll } from "../components/ui/parallax-scroll.tsx";
 
 export function GalleryShow() {
-  const [img, setImg] = useState()
-  console.log(img)
+  const [img, setImg] = useState();
+  console.log(img);
 
-  useEffect(() =>{
-    fetchImg()
-  },[])
- const fetchImg = async() => {
-  const data = await fetch("https://ik.imagekit.io/wratuu6c1/dashboard/media-collections/Office")
-  const json = await data.json()
-  setImg(json)
-
- }
+  useEffect(() => {
+    fetchImg();
+  }, []);
+  const fetchImg = async () => {
+    const data = await fetch(
+      "https://ik.imagekit.io/wratuu6c1/dashboard/media-collections/Office"
+    );
+    const json = await data.json();
+    setImg(json);
+  };
   const products = [
     {
       name: "Products",
       images: [
-        "https://ik.imagekit.io/wratuu6c1/IMG_2684.jpg?updatedAt=1735361971674",
-        "https://ik.imagekit.io/wratuu6c1/IMG_2681.jpg?updatedAt=1735361971587",
-        "https://ik.imagekit.io/wratuu6c1/IMG_2678.jpg?updatedAt=1735361971474",
-        "https://ik.imagekit.io/wratuu6c1/IMG_2676.jpg?updatedAt=1735361971413",
-        "https://ik.imagekit.io/wratuu6c1/IMG_2677.jpg?updatedAt=1735361971372",
-        "https://ik.imagekit.io/wratuu6c1/IMG_2679.jpg?updatedAt=1735361971290",
-        "https://ik.imagekit.io/wratuu6c1/IMG_2672.jpg?updatedAt=1735360101822",
+        "https://ik.imagekit.io/wratuu6c1/blower%20manapparai.PNG?updatedAt=1735291707050",
+        "https://ik.imagekit.io/wratuu6c1/blower%203.PNG?updatedAt=1735291707025",
+        "https://ik.imagekit.io/wratuu6c1/IMG_1111.JPEG?updatedAt=1735290483064",
+        "https://ik.imagekit.io/wratuu6c1/IMG_1110.JPEG?updatedAt=1735290483066",
+        "https://ik.imagekit.io/wratuu6c1/Blower%20Air%20seperator%20Kappas%20cleaner%20front%20(1).JPEG?updatedAt=1735291706944",
+        "https://ik.imagekit.io/wratuu6c1/IMG_1035.JPEG?updatedAt=1735289152986",
+        "https://ik.imagekit.io/wratuu6c1/IMG_1075.JPEG?updatedAt=1735289004158",
+        "https://ik.imagekit.io/wratuu6c1/Baling%20Machine%201.jpg?updatedAt=1735288594343",
+        "https://ik.imagekit.io/wratuu6c1/IMG_1089.JPEG?updatedAt=1735271418463",
+        "https://ik.imagekit.io/wratuu6c1/IMG_1143.JPEG?updatedAt=1735271418455",
+        "https://ik.imagekit.io/wratuu6c1/IMG_1116.JPEG?updatedAt=1735271418396",
       ],
     },
-    { name: "Machinaries", images: ["<URL4>", "<URL5>", "<URL6>"] },
-    { name: "SriTex", images: ["<URL7>", "<URL8>", "<URL9>"] },
+    {
+      name: "Machinaries",
+      images: [
+        "https://ik.imagekit.io/wratuu6c1/machinaries/IMG_2689.jpg?updatedAt=1735380286197",
+        "https://ik.imagekit.io/wratuu6c1/machinaries/IMG_2688.jpg?updatedAt=1735380283468",
+        "https://ik.imagekit.io/wratuu6c1/machinaries/IMG_2687.jpg?updatedAt=1735380286113",
+        "https://ik.imagekit.io/wratuu6c1/machinaries/IMG_2690.jpg?updatedAt=1735380286107",
+        "https://ik.imagekit.io/wratuu6c1/machinaries/IMG_2686.jpg?updatedAt=1735380285875",
+        "https://ik.imagekit.io/wratuu6c1/machinaries/IMG_2691.jpg?updatedAt=1735380286116"
+      ],
+    },
+    {
+      name: "SriTex",
+      images: [
+        "https://ik.imagekit.io/wratuu6c1/IMG_2667.jpg?updatedAt=1735360101798",
+        "https://ik.imagekit.io/wratuu6c1/IMG_2670.jpg?updatedAt=1735360101543",
+        "https://ik.imagekit.io/wratuu6c1/IMG_2666.jpg?updatedAt=1735360101528",
+        "https://ik.imagekit.io/wratuu6c1/IMG_2668.jpg?updatedAt=1735360100850",
+        "https://ik.imagekit.io/wratuu6c1/IMG_2664.jpg?updatedAt=1735360100821",
+        "https://ik.imagekit.io/wratuu6c1/IMG_2673.jpg?updatedAt=1735360100788",
+        "https://ik.imagekit.io/wratuu6c1/IMG_2669.jpg?updatedAt=1735360100730",
+      ],
+    },
     {
       name: "Office",
       images: [
@@ -81,10 +107,12 @@ export function GalleryShow() {
         <div
           className="w-12/12 flex flex-col justify-center items-center mb-5"
           key={index}
-          onClick={() => handleExpand(index)}
         >
           {/* The div with the border and content */}
-          <div className="w-10/12 h-16 border-4 border-green-700 flex flex-row justify-between items-center rounded-md p-3">
+          <div
+            className="w-10/12 h-16 border-4 border-green-700 flex flex-row justify-between items-center rounded-md p-3"
+            onClick={() => handleExpand(index)}
+          >
             <div className="text-lg font-extrabold cursor-pointer w-full sm:w-2/3 md:w-1/2 ml-3 truncate">
               {product.name}
             </div>
