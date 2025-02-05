653cccd4980b52129c3597a57a9aa6ff84ebf9c5
Pavithrank02 <pavithrank02@gmail.com>
2025-02-05 14:48:45 +0530

    FAQ Ui updated

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,25 +1,30 @@
 // src/FAQ.js
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { IconCaretDownFilled, IconCaretUpFilled } from "@tabler/icons-react";
 
 const FAQ = () => {
   const [activeIndex, setActiveIndex] = useState(null);
 
   const faqData = [
     {
-      question: 'What is your return policy?',
-      answer: 'Our return policy allows you to return products within 30 days of purchase. Make sure the product is in its original condition.',
+      question: "What is your return policy?",
+      answer:
+        "Our return policy allows you to return products within 30 days of purchase. Make sure the product is in its original condition.",
     },
     {
-      question: 'Do you offer international shipping?',
-      answer: 'Yes, we offer international shipping to select countries. Shipping fees and delivery times vary by location.',
+      question: "Do you offer international shipping?",
+      answer:
+        "Yes, we offer international shipping to select countries. Shipping fees and delivery times vary by location.",
     },
     {
-      question: 'How do I track my order?',
-      answer: 'Once your order is shipped, you will receive a tracking number via email. You can use this number to track your order on our website.',
+      question: "How do I track my order?",
+      answer:
+        "Once your order is shipped, you will receive a tracking number via email. You can use this number to track your order on our website.",
     },
     {
-      question: 'What warranties do you offer on machinery?',
-      answer: 'We offer a one-year warranty on all of our machinery. Extended warranties are available upon request.',
+      question: "What warranties do you offer on machinery?",
+      answer:
+        "We offer a one-year warranty on all of our machinery. Extended warranties are available upon request.",
     },
   ];
 
@@ -37,15 +42,32 @@ const FAQ = () => {
           const isActive = activeIndex === index;
 
           return (
-            <div key={index} className="border-b border-customBgColor">
-              <button
-                onClick={() => toggleAnswer(index)}
-                className="w-full text-left py-4 px-6 text-xl font-semibold text-gray-800 hover:bg-customBgColor-bg transition duration-300 ease-in-out transform hover:scale-105"
-              >
-                {item.question}
-              </button>
+            <div
+              key={index}
+              className="border-b border-customBgColor hover:bg-customBgColor-bg transition duration-300 ease-in-out transform hover:scale-105 "
+            >
               <div
-                className={`overflow-hidden transition-all duration-500 ease-in-out transform ${isActive ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'} `}
+                className="flex items-center justify-between cursor-pointer"
+                onClick={() => toggleAnswer(index)}
+              >
+                <div>
+                  <button className="w-full text-left py-4 px-6 text-xl font-semibold text-gray-800 ">
+                    {item.question}
+                  </button>
+                </div>
+                <div>
+                  {isActive ? (
+                    <IconCaretUpFilled />
+                  ) : (
+                    <IconCaretDownFilled></IconCaretDownFilled>
+                  )}
+                </div>
+              </div>
+
+              <div
+                className={`overflow-hidden transition-all duration-500 ease-in-out transform ${
+                  isActive ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+                } `}
               >
                 <div className="p-6 bg-gray-50 text-gray-600">
                   <p>{item.answer}</p>
