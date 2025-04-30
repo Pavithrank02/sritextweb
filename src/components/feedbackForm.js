afaafeec242a1124869d3c65ed5aeaf11008f00c
Pavithrank02 <pavithrank02@gmail.com>
2025-04-30 11:23:24 +0530

    xx

--- src/components/feedbackForm.js
+++ src/components/feedbackForm.js
@@ -14,7 +14,7 @@ const FeedbackForm = () => {
       [name]: value,
     }));
   };
-//sd
+//sds
   const handleSubmit = (e) => {
     e.preventDefault();
     // For now, log the data to the consoles
